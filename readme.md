87c8c49dfb27822a704444d7b3369636a18f7420
a7f3 <124642739+a7f3@users.noreply.github.com>
2023-10-31 00:21:52 +1100

    Game

---
+++ readme.md
@@ -0,0 +1,2 @@
+# decrement
+small game where the objective is to set your hp to greater than 1000, most likely using a memory editors
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    let mut hp: i32 = 100;
+    let mut line = String::new();
+
+    while hp < 1000 {
+        println!("hp is currently {}", hp);
+
+
+        println!("Next? ");
+
+
+        let _ = io::stdin().read_line(&mut line);
+
+        hp -= rand::thread_rng().gen_range(2..5);
+
+        if hp <= 0 {
+            println!("Oh no you died! reviving you!");
+            hp = 100;
+        }
+    }
+    println!("You win!");
+}
