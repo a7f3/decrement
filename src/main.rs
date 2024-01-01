c53ad6d4fdfb215153df043278b8a94606ec2ff2
a7f3 <124642739+a7f3@users.noreply.github.com>
2024-01-01 17:26:08 +1100

    Printing out pid at program start

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use rand::Rng;
 use std::io;
+use std::process;
 
 fn main() {
     let mut hp: i32 = 100;
     let mut line = String::new();
 
+    println!("pid: {}", process::id());
+
     while hp < 1000 {
         println!("hp is currently {}", hp);
 
-
         println!("Next? ");
-
 
         let _ = io::stdin().read_line(&mut line);
 
